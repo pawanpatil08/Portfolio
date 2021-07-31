ed80407ab8f006479bf98f33f400640847d8b38a
Pawan Patil <pwnpatil08@gmail.com>
2021-07-31 18:07:47 +0530

    change

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/portfolio'));
+app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/portfolio/'}),
+    res.sendFile('index.html', {root: 'dist'}),
 );
 
 // Start the app by listening on the default Heroku port
@@ -19,10 +19,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/portfolio'));
+app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/portfolio/'}),
+    res.sendFile('index.html', {root: 'dist'}),
 );
 
 // Start the app by listening on the default Heroku port
